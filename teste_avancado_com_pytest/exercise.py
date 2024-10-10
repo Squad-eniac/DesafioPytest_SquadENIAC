0e81af4aa5f466bcc164b6d69d7872742828019c
Rosana de Souza Brito <110073324+RosanaBrito@users.noreply.github.com>
2024-10-10 02:12:18 -0300

    Adicionando exercise.py
    Adicionando exercise.py do Teste Avançado.

---
+++ teste_avancado_com_pytest/exercise.py
@@ -0,0 +1,20 @@
+def str_to_bool(value):
+    """
+    Converte uma string em um valor booleano.
+    A função str_to_bool segue a mesma ideia de transformação de strings para booleanos, lidando com exceções e com entradas não esperadas.
+    """
+
+    try:
+        value = value.lower()
+    except AttributeError:
+        raise AttributeError(f'{value} deve ser uma string')
+
+    true_values = ['y', 'yes']
+    false_values = ['n', 'no']
+
+    if value in true_values:
+        return True
+    elif value in false_values:
+        return False
+    else:
+        raise ValueError(f'{value} não é um valor booleano válido')
