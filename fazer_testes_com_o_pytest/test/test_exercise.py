e89225de1203af7f9826e1229a1336bedecbc396
Ana Julia Rauber <anajuliarauber@hotmail.com>
2024-10-12 09:51:03 -0300

    feat: adiciona teste para comandos que não são listas

--- fazer_testes_com_o_pytest/test/test_exercise.py
+++ fazer_testes_com_o_pytest/test/test_exercise.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pytest
 
 # Adiciona o caminho para o diretório raiz
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
@@ -20,3 +21,8 @@ class TestAdminCommand:
         result = admin_command(self.command(), sudo=True)
         expected = ["sudo"] + self.command()
         assert result == expected
+        
+    def test_non_list_commands(self):
+        with pytest.raises(TypeError) as error:
+            admin_command("some command", sudo=True)
+        assert error.value.args[0] == "was expecting command to be a list, but got a <class 'str'>"
