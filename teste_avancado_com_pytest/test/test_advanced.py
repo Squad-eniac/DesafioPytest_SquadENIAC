d4806aed9dba8099dc20a495700e3224551a2a2a
marina6coneto <santoscmarina@hotmail.com>
2024-10-14 15:33:19 -0300

    import sys and os

--- teste_avancado_com_pytest/test/test_advanced.py
+++ teste_avancado_com_pytest/test/test_advanced.py
@@ -1,5 +1,7 @@
 import pytest
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from teste_avancado_com_pytest.exercise import str_to_bool
 
